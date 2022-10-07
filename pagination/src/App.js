1d1346a24a44698d1ee02939bde6c78eaba9394f
MacbookNikkiii <MacbookProRetina@Admins-MacBook-Pro.local>
2022-10-07 11:58:02 +0700

    added files

---
+++ pagination/src/App.js
@@ -0,0 +1,79 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import { useFetch } from "./hi/useFetch";
+import Follower from "./hi/Follower";
+
+function App() {
+  const { loading, data } = useFetch();
+  const [page, setPage] = useState(0);
+  const [followers, setFollowers] = useState([]);
+  console.log(data);
+
+  //set up useEffect when the page load to have it show just a certain followers
+  useEffect(() => {
+    if (loading) return;
+    setFollowers(data[page]);
+  }, [loading, page]);
+
+  const handlePage = (index) => {
+    setPage(index);
+  };
+  const prevPage = () => {
+    setPage((oldPage) => {
+      let prevPage = oldPage - 1;
+      if (prevPage < 0) {
+        prevPage = data.length - 1;
+      }
+      return prevPage;
+    });
+  };
+  const nextPage = () => {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1;
+      if (nextPage > data.length - 1) {
+        nextPage = 0;
+      }
+      return nextPage;
+    });
+  };
+  return (
+    <main className="App">
+      <div className="section-title">
+        <h1>{loading ? "loading..." : "Pagination"}</h1>
+        <div className="underline"></div>
+      </div>
+      <section className="followers">
+        <div className="container">
+          {followers.map((follower) => {
+            return <Follower key={follower.id} {...follower} />;
+          })}
+        </div>
+        {/* set if finished loading then show the btn */}
+        {!loading && (
+          <div className="btn-container">
+            <button className="prev-btn" onClick={prevPage}>
+              prev
+            </button>
+            {data.map((item, index) => {
+              return (
+                <button
+                  className={`page-btn ${
+                    index === page ? "active-btn" : null
+                  } `}
+                  key={index}
+                  onClick={() => handlePage(index)}>
+                  {index + 1}
+                </button>
+              );
+            })}
+            <button className="next-btn" onClick={nextPage}>
+              next
+            </button>
+          </div>
+        )}
+      </section>
+    </main>
+  );
+}
+
+export default App;
